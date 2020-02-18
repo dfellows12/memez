12226140cbd6bded3f3aecd1dfd70cf9df89b73a
David Fellows <dfellows12@gmail.com>
2020-02-18 17:50:34 -0500

    Added route for likes

--- socialapp-functions/functions/index.js
+++ socialapp-functions/functions/index.js
@@ -3,7 +3,7 @@ const app = require('express')()
 const FBAuth = require('./util/fbAuth')
 
 const { getAllMemes, postOneMeme } = require('./handlers/memes')
-const { signup, login, uploadImage, addUserDetails } = require('./handlers/users')
+const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users')
 
 
 //Scream routes
@@ -15,6 +15,7 @@ app.post('/signup', signup)
 app.post('/login', login)   
 app.post('/user/image', FBAuth, uploadImage)
 app.post('/user', FBAuth, addUserDetails)
+app.get('/user', FBAuth, getAuthenticatedUser)
 
 
 exports.api = functions.https.onRequest(app);
